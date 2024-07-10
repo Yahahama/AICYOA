ff43404ee41d0db346b417d8599048782ab5f36e
Amika <92111982+Yahahama@users.noreply.github.com>
2024-07-10 17:41:43 -0400

    Set up a test run to generate options, and it went surprisingly well! Did not immediately crash! Did not work entirely as intended, but that's the fault of bad prompt engineering and not the code itself.

--- aistorycreator.py
+++ aistorycreator.py
@@ -1,6 +1,8 @@
 from gpt4all import GPT4All
 import json
 
+currentChoiceToFillOut = 0
+
 model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", "C:/Users/rokso/AppData/Local/nomic.ai/GPT4All/")
 print("Model loaded!")
 
@@ -9,3 +11,12 @@ storypath = r"C:\Users\rokso\OneDrive\Documents\Code\AICYOA\story.json"
 with open(storypath, "r") as file:
     storydata = json.load(file)
 
+print("Story loaded!")
+
+system_template = "An uncompleted choose-your-own-adventure interactive story that needs to be filled in with story when a colon is followed by nothing. For example, 'Choice 1: {fill in story after colon'"
+prompt_template = "{0}\Choice " + str(currentChoiceToFillOut) + ": "
+
+with model.chat_session(system_template, prompt_template):
+    print(storydata["storyNodes"][0]["text"])
+    response = model.generate(storydata["storyNodes"][0]["text"])
+    print(response)
