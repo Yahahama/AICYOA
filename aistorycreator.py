321a3d4ac3086f96ca349a4f9704d8f9b8a37ae5
Amika <92111982+Yahahama@users.noreply.github.com>
2024-07-20 21:27:23 -0400

    Wrote an untested first version of the recursive function that repeatedly prompts the AI to create story.

--- aistorycreator.py
+++ aistorycreator.py
@@ -46,30 +46,53 @@ with open(storypath, "r") as file:
 printGreen("JSON loaded!")
 
 printGreen("What would you like the initial scenario to be? (Leave blank for the default scenario):")
-storyText = input()
-if storyText == "":
-    storyText = "You are in a dark room. You can't see anything."
+initialStory = input()
+if initialStory == "":
+    initialStory = "You are in a dark room. You can't see anything."
     delete_last_lines(1)
-    print(storyText)
+    print(initialStory)
 
 printGreen("How many choices deep do you want your game to be? (Note: each layer deeper will take 3x as long to generate.)")
 depth = int(input())
 
-currentID = ""
-
 system_template = "A helpful generative AI that specializes in writing stories is chatting with a curious user who wants it to write some content l his choose-your-own-adventure game."
 prompt_template = "USER: Hi, I've got a snippet of a CYOA game's story here: \"{0}\" I need you to write three choices to give the player at this point, each choice surrounded by quotation marks and separated by a whitespace. After all three are completed, write three corresponding story continuations each surrounded by quotes and seperated by a whitespace.\nAI: "
 
-with model.chat_session(system_template, prompt_template):
-    for i in range(depth):
-        response = model.generate(storyText, max_tokens=400, temp=.75)
+genChoices = []
+
+def createStory(id=''):
+    currentDepth = len(id)
+    if currentDepth >= depth: return
+    if currentDepth == depth - 1:
+        prompt_template = "USER: Hi, I've got a snippet of a CYOA game's story here: \"{0}\" I need you to write three choices to give the player at this point, each choice surrounded by quotation marks and separated by a whitespace. After all three are completed, write three corresponding story conclusions each surrounded by quotes and seperated by a whitespace.\nAI: "
+    with model.chat_session(system_template, prompt_template):
+        response = model.generate(initialStory, max_tokens=400, temp=.75)
+        prevGenChoices = genChoices
         genChoices = extractQuote(response, 6)[:3]
         genStories = extractQuote(response, 6)[3:]
-        if currentID == '':
-            newNode = {
-                "text": storyText,
-                "id": "initial",
-            }
-        else: newNode = {
-            "text": 
+    newNode = {}
+    if currentDepth == 0:
+        newNode.update({"text": initialStory})
+    else:
+        newNode.update({"text": prevGenChoices[id[-1]]})
+    newNode.update({"choices": [
+        {
+            "text": genChoices[0],
+            "nextNodeID": id + 0
+        },
+        {
+            "text": genChoices[1],
+            "nextNodeID": id + 1
+        },
+        {
+            "text": genChoices[2],
+            "nextNodeID": id + 2
         }
+    ]})
+    storydata.update(newNode)
+    createStory(id + '0')
+    createStory(id + '1')
+    createStory(id + '2')
+
+    with open(storypath, "w") as file:
+        json.dump(storydata, file)
