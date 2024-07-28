257e9f50a325a4031c17b27a18730930949ceb4b
Amika <92111982+Yahahama@users.noreply.github.com>
2024-07-28 19:42:30 +0500

    attempted to debug aistorycreator, bug where only the last generation is shown in story.json

--- aistorycreator.py
+++ aistorycreator.py
@@ -1,6 +1,7 @@
 from gpt4all import GPT4All
 import json
 from sys import platform
+from time import sleep
 import os
 
 def extractQuote(text, n=1):
@@ -24,7 +25,7 @@ def delete_last_lines(n=1):
     ERASE_LINE = '\x1b[2K' 
     for _ in range(n): 
         stdout.write(ERASE_LINE) 
-        stdout.write(CURSOR_UP_ONE) 
+        stdout.write(CURSOR_UP_ONE)
 
 def printGreen(text, *args, **kwargs): print("\033[92m{}\033[00m".format(text), *args, **kwargs)
 
@@ -40,59 +41,68 @@ printGreen("Model loaded!")
 
 storypath = os.path.join(os.path.dirname(__file__), "story.json")
 
-with open(storypath, "r") as file:
-    storydata = json.load(file)
+storydata = {}
 
 printGreen("JSON loaded!")
 
 printGreen("What would you like the initial scenario to be? (Leave blank for the default scenario):")
-initialStory = input()
+# initialStory = input()
+initialStory = ""
 if initialStory == "":
     initialStory = "You are in a dark room. You can't see anything."
     delete_last_lines(1)
     print(initialStory)
 
 printGreen("How many choices deep do you want your game to be? (Note: each layer deeper will take 3x as long to generate.)")
-depth = int(input())
+# depth = int(input())
+depth = 2
+
+printGreen("Generating story. This may take a while.")
 
 system_template = "A helpful generative AI that specializes in writing stories is chatting with a curious user who wants it to write some content l his choose-your-own-adventure game."
-prompt_template = "USER: Hi, I've got a snippet of a CYOA game's story here: \"{0}\" I need you to write three choices to give the player at this point, each choice surrounded by quotation marks and separated by a whitespace. After all three are completed, write three corresponding story continuations each surrounded by quotes and seperated by a whitespace.\nAI: "
 
-genChoices = []
-
-def createStory(id=''):
+def createStory(id='', genStories=[]):
+    newNode.update(id)
     currentDepth = len(id)
     if currentDepth >= depth: return
     if currentDepth == depth - 1:
         prompt_template = "USER: Hi, I've got a snippet of a CYOA game's story here: \"{0}\" I need you to write three choices to give the player at this point, each choice surrounded by quotation marks and separated by a whitespace. After all three are completed, write three corresponding story conclusions each surrounded by quotes and seperated by a whitespace.\nAI: "
-    with model.chat_session(system_template, prompt_template):
-        response = model.generate(initialStory, max_tokens=400, temp=.75)
-        prevGenChoices = genChoices
-        genChoices = extractQuote(response, 6)[:3]
-        genStories = extractQuote(response, 6)[3:]
-    newNode = {}
-    if currentDepth == 0:
-        newNode.update({"text": initialStory})
     else:
-        newNode.update({"text": prevGenChoices[id[-1]]})
-    newNode.update({"choices": [
-        {
-            "text": genChoices[0],
-            "nextNodeID": id + 0
-        },
-        {
-            "text": genChoices[1],
-            "nextNodeID": id + 1
-        },
-        {
-            "text": genChoices[2],
-            "nextNodeID": id + 2
-        }
-    ]})
+        prompt_template = "USER: Hi, I've got a snippet of a CYOA game's story here: \"{0}\" I need you to write three choices to give the player at this point, each choice surrounded by quotation marks and separated by a whitespace. After all three are completed, write three corresponding story continuations each surrounded by quotes and seperated by a whitespace.\nAI: "
+    newNode = {}
+    with model.chat_session(system_template, prompt_template):
+        if currentDepth == 0:
+            newNode.update({"text": initialStory})
+        else:
+            newNode.update({"text": genStories[int(id[-1])]})
+        if currentDepth < depth - 1:
+            response = model.generate(initialStory, max_tokens=400, temp=.75)
+            genChoices = extractQuote(response, 6)[:3]
+            genStories = extractQuote(response, 6)[3:]
+            newNode.update({"choices": [
+                {
+                    "text": genChoices[0],
+                    "nextNodeID": id + '0'
+                },
+                {
+                    "text": genChoices[1],
+                    "nextNodeID": id + '1'
+                },
+                {
+                    "text": genChoices[2],
+                    "nextNodeID": id + '2'
+                }
+            ]})
     storydata.update(newNode)
-    createStory(id + '0')
-    createStory(id + '1')
-    createStory(id + '2')
+    createStory(id + '0', genStories)
+    createStory(id + '1', genStories)
+    createStory(id + '2', genStories)
 
-    with open(storypath, "w") as file:
-        json.dump(storydata, file)
+createStory()
+
+printGreen("Story generated!")
+
+with open(storypath, "w") as file:
+    json.dump(storydata, file)
+
+printGreen("Story saved!")
