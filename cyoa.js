ce44d81cc0b807a9abbff8f1eae03d69c08d9b04
Amika <92111982+Yahahama@users.noreply.github.com>
2024-08-09 17:08:19 +0500

    Added

--- cyoa.js
+++ cyoa.js
@@ -1,4 +1,5 @@
 let currentID = '';
+let storyData;
 const storyURL = "https://yahahama.github.io/AICYOA/story.json";
 
 fetch(storyURL)
@@ -9,8 +10,35 @@ fetch(storyURL)
         return response.json();
     })
     .then(data => {
-        console.log(data);
+        storyData = data;
     })
     .catch(error => {
         console.error('Error loading story.json:', error);
     });
+
+text = document.getElementById('text');
+choice0 = document.getElementById('choice0');
+choice1 = document.getElementById('choice1');
+choice2 = document.getElementById('choice2');
+
+function updateScreen() {
+    text.innerHTML = storyData[currentID].text;
+    choice0.innerHTML = storyData[currentID].choices[0].text;
+    choice1.innerHTML = storyData[currentID].choices[1].text;
+    choice2.innerHTML = storyData[currentID].choices[2].text;
+}
+
+updateScreen();
+
+addEventListener('click', function (event) {
+    if (event.target === choice0) {
+        currentID = storyData[currentID].choices[0].nextID;
+        updateScreen();
+    } else if (event.target === choice1) {
+        currentID = storyData[currentID].choices[1].nextID;
+        updateScreen();
+    } else if (event.target === choice2) {
+        currentID = storyData[currentID].choices[2].nextID;
+        updateScreen();
+    }
+})
