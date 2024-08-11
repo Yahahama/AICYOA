0b6a0caa5fce97447c9447f3d41d8d63c2de4e32
Amika <92111982+Yahahama@users.noreply.github.com>
2024-08-11 20:53:42 +0500

    Debugging cyoa.js

--- cyoa.js
+++ cyoa.js
@@ -1,5 +1,5 @@
 let currentID = '';
-let storyData;
+let storyData = null;
 const storyURL = "https://yahahama.github.io/AICYOA/story.json";
 
 fetch(storyURL)
@@ -11,8 +11,7 @@ fetch(storyURL)
     })
     .then(data => {
         storyData = data;
-        console.log("Story loaded successfully", storyData);
-        updateScreen(storyData);
+        updateScreen();
     })
     .catch(error => {
         console.error('Error loading story.json:', error);
@@ -23,34 +22,27 @@ choice0 = document.getElementById('choice0');
 choice1 = document.getElementById('choice1');
 choice2 = document.getElementById('choice2');
 
-function updateScreen(storyData) {
+function updateScreen() {
     if (currentID === '') {
         base10ID = 0;
     } else {
         base10ID = parseInt(currentID, 3) + 1;
     }
-    console.log("Beginning crazy logs!")
-    console.log(base10ID);
-    console.log(storyData);
-    console.log(storyData[base10ID]);
-    console.log(storyData[base10ID].text);
     text.innerHTML = storyData[base10ID].text;
     choice0.innerHTML = storyData[base10ID].choices[0].text;
     choice1.innerHTML = storyData[base10ID].choices[1].text;
     choice2.innerHTML = storyData[base10ID].choices[2].text;
-    console.log("updateScreen ran successfully")
 }
 
-console.log("Got to event listener!")
 addEventListener('click', function (event) {
     if (event.target === choice0) {
         currentID = storyData[base10ID].choices[0].nextNode;
-        updateScreen(storyData);
+        updateScreen();
     } else if (event.target === choice1) {
         currentID = storyData[base10ID].choices[1].nextNode;
-        updateScreen(storyData);
+        updateScreen();
     } else if (event.target === choice2) {
         currentID = storyData[base10ID].choices[2].nextNode;
-        updateScreen(storyData);
+        updateScreen();
     }
 })
